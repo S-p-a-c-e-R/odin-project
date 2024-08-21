1865ef427f607fff332e2e9b912589d383fa361e
S-p-a-c-e-R <173574669+S-p-a-c-e-R@users.noreply.github.com>
2024-08-22 00:37:30 +0300

    Wrote logic for computer choice and human input

--- foundations/javascript/script.js
+++ foundations/javascript/script.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+
+  if (choice === 0) {
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
+}
+console.log(getComputerChoice());
+
+let input = prompt("Rock, Paper or Scissor?");
+let humanChoice = input.toLowerCase();
+
+function getHumanChoice() {
+  if (humanChoice === "rock") {
+    return 0;
+  } else if (humanChoice === "paper") {
+    return 1;
+  } else if (humanChoice === "scissor") {
+    return 2;
+  } else {
+    return "Make a choice";
+  }
+}
+console.log(getHumanChoice(humanChoice));
